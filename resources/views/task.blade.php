5cbce65b419cb532bf6588eb4c8cd7ee64cc9e1f
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2022-03-01 00:25:40 +1000

    Update style

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -1,13 +1,13 @@
 <div class="mx-2 mt-1 px-1 w-full">
-    <div>
+    <div class="space-x-1">
         <span class="text-white italic">{{ $name = $task->getPurpose() }}</span>
-        <span class="px-1 text-gray-500">{{ str_repeat('.', $width - mb_strlen($name) - mb_strlen($status) - 8) }}</span>
+        <span class="text-blue-600">{{ str_repeat('.', $width - mb_strlen($name) - mb_strlen($status) - 8) }}</span>
         <span class="{{ $color }}">{{ $status }}</span>
     </div>
 
-    <div>
+    <div class="space-x-2">
         @if ($reason = $task->getStatusReason())
-            <span class="text-white font-bold mr-2">⤷</span>
+            <span class="text-white font-bold">⤷</span>
             <span class="italic {{ $color }}">{{ $reason }}</span>
         @endif
     </div>
