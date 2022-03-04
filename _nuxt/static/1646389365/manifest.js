3ec05747debcb65bee1a2243cafd450fdb5f9f2d
cerbero90 <cerbero90@users.noreply.github.com>
2022-03-04 10:22:46 +0000

    deploy: 05102018f0d607c6de021d205eb3c32872c6ab17

---
+++ _nuxt/static/1646389365/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fdeveloping-file-creator","\u002Fquerying-summary","\u002Fdeveloping-files-editor","\u002Fcustomizing-stubs","\u002Finstallation","\u002Fbuilding-printer","\u002Fdeveloping-task","\u002Fcustomizing-views","\u002Fgenerating-tasks"]})
